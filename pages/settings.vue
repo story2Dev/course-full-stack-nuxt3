4065e69cb7bd5bd548af8eced254591a6ef6ab2f
Mark AI <chanthavong_12@hotmail.com>
2023-11-18 19:12:43 +0700

    add setting

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,9 +1,78 @@
 <template>
-    <div>
-        <nav>
-            <nuxt-link to="/settings/account">Account</nuxt-link> |
-            <nuxt-link to="/settings/general">General</nuxt-link> 
-            <NuxtPage />
-        </nav>
-    </div>
+  <div
+    class="flex max-w-2xl w-full mx-auto gap-4 border p-4 rounded-xl min-h-[300px]"
+  >
+    <nav class="w-64">
+      <div class="relative">
+        <input
+          type="text"
+          class="mb-2 w-full border rounded-lg px-6 text-sm py-1"
+          placeholder="search"
+        />
+        <Icon
+          name="system-uicons:search"
+          class="absolute left-2 mt-2 text-grey-dark"
+        ></Icon>
+      </div>
+      <div class="my-2 mb-4 flex gap-2">
+        <div>
+          <img
+            class="w-12 w-12 rounded-full"
+            src="https://avatars.githubusercontent.com/u/739984?v=4"
+            alt=""
+          />
+        </div>
+        <div>
+          <p class="">
+            <h1 class="font-bold text-sm">
+                Mark AI
+            </h1>
+            <small>Apple ID</small>
+          </p>
+         
+        </div>
+      </div>
+      <ul class="w-full flex flex-col">
+        <li class="w-full">
+          <nuxt-link
+            to="/settings/account"
+            active-class="bg-green-100"
+            class="hover:bg-slate-100 w-full rounded-lg py-1 px-2  flex gap-2 items-center text-sm"
+          >
+            <Icon name="system-uicons:user" class="text-grey-dark" size="20"></Icon>
+            <span>Account</span>
+          </nuxt-link>
+        </li>
+        <li class="w-full">
+          <nuxt-link
+            to="/settings/general"
+            active-class="bg-green-100"
+            class="hover:bg-slate-100 w-full rounded-lg py-1 px-2  flex gap-2 items-center text-sm"
+          >
+            <Icon name="system-uicons:settings" class="text-grey-dark" size="20"></Icon>
+            <span>General</span>
+          </nuxt-link>
+        </li>
+        <li class="w-full">
+          <nuxt-link
+            to="/settings/notification"
+            active-class="bg-green-100"
+            class="hover:bg-slate-100 w-full rounded-lg py-1 px-2 flex gap-2 items-center text-sm"
+          >
+            <Icon name="system-uicons:bell" class="text-grey-dark" size="20"></Icon>
+            <span>Notification</span>
+          </nuxt-link>
+        </li>
+       
+      </ul>
+    </nav>
+    <main class="flex-grow">
+      <h2 class="text-xl mb-2 flex gap-2 items-center">
+        <Icon name="system-uicons:chevron-left" class="text-grey-dark" size="20"></Icon>
+        <Icon name="system-uicons:chevron-right" class="text-grey-dark" size="20"></Icon>
+        <span>Settings</span>
+      </h2>
+      <NuxtPage />
+    </main>
+  </div>
 </template>
--- pages/settings/account.vue
+++ pages/settings/account.vue
@@ -1,5 +1,63 @@
 <template>
-    <div>
+    <div class="w-full">
         Account
+        <div class="my-2 mb-4 flex gap-2">
+        <div>
+          <img
+            class="w-12 h-12 rounded-full"
+            src="https://avatars.githubusercontent.com/u/739984?v=4"
+            alt=""
+          />
+        </div>
+        <div class="flex flex-grow justify-between items-center">
+           <div>
+            <h1 class="font-bold text-sm">
+                Mark AI
+            </h1>
+            <small>Apple ID</small>
+           </div>
+          <button>Edit</button>
+        </div>
+      </div>
+        <ul class="border rounded-lg py-4 px-4 divide-y">
+            <li class="flex justify-between py-1">
+                <div>
+                    <h3 class="font-bold text-sm">Name</h3>
+                    <p class="text-sm">Mark AI</p>
+                </div>
+                <button class="text-sm">Edit</button>
+            </li>
+            <li class="flex justify-between py-1">
+                <div>
+                    <h3 class="font-bold text-sm">Email</h3>
+                    <p class="text-sm">mark@email.com</p>
+                </div>
+                <button class="text-sm">Edit</button>
+            </li>
+        </ul>
+
+        <ul class="border rounded-lg py-4 px-4 divide-y mt-6 mb-4">
+            <li class="flex justify-between py-2">
+                <div>
+                    <h3 class="font-bold text-sm">Language</h3>
+                </div>
+                <button class="text-sm">Edit</button>
+            </li>
+            <li class=" py-2">
+                <div>
+                    <h3 class="font-bold text-sm">Theme</h3>
+                </div>
+                <ol class="mt-2 flex gap-2">
+                    <li class="border rounded-lg p-4 px-6 flex items-center gap-2">
+                        <Icon name="system-uicons:sun" class="text-yellow-500" size="20"></Icon>
+                        Light
+                    </li>
+                    <li class="border rounded-lg p-4 px-6 flex gap-2 items-center">
+                        <Icon name="system-uicons:moon" class="text-yellow-500" size="20"></Icon>
+                        Dark
+                    </li>
+                </ol>
+            </li>
+        </ul>
     </div>
 </template>
